a77c4cc28e2da8af6fb481e7457d1cb5f0a36894
P3D\rbauvin <raphaelbauvin@yahoo.fr>
2021-03-18 10:49:45 +0100

    Brick connected to Game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public static Game Instance;
+    public TypeBrick active;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
